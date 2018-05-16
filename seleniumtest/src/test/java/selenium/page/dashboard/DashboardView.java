dfa3928827b85e831e48b711c0e5131fefad745a
mgegaj <mgegaj@pc-mgegaj.ikub.local>
2018-05-16 12:37:42 +0200

    UserAdministration Test: clearButtonTest & submitButtonTest

--- seleniumtest/src/test/java/selenium/page/dashboard/DashboardView.java
+++ seleniumtest/src/test/java/selenium/page/dashboard/DashboardView.java
@@ -1,14 +1,43 @@
 package selenium.page.dashboard;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static selenium.tests.utils.Constants.*;
 
 import selenium.page.AbstractView;
+import selenium.page.login.LoginView;
+
 
 public class DashboardView extends AbstractView {
+	
+	private WebElement menuButton;
+	private WebElement usersRolesButton;
+	private WebElement userAdministrationButton;
+	
+	private LoginView loginView;
+	
+	private By menuButtonLocator = By.id("menu-button");
+	private By usersRolesButtonLocator = By.id("users_roles");
+	private By userAdministrationButtonLocator = By.id("user_administration");
+	
 
-	public DashboardView(WebDriver webDriver) {
+	public DashboardView(WebDriver webDriver) throws InterruptedException {
 		super(webDriver);
-		
+		loginView = new LoginView(webDriver);
+		loginView.login(USR, PWD);
+		menuButton = webDriver.findElement(menuButtonLocator);
+		usersRolesButton = webDriver.findElement(usersRolesButtonLocator);
+		userAdministrationButton = webDriver.findElement(userAdministrationButtonLocator);	
 	}
+	
+	public void navigateToUserAdministration() throws InterruptedException {
+			menuButton.click();
+			Thread.sleep(1000);
+			usersRolesButton.click();
+			Thread.sleep(1000);
+			userAdministrationButton.click();
+		}
 
 }
