9e34cd0ee2939df1f2f1e67d1594922195be17f8
mgegaj <mgegaj@pc-mgegaj.ikub.local>
2018-05-16 08:56:46 +0200

    test

--- seleniumtest/src/test/java/seleniumtest/DriverUtil.java
+++ seleniumtest/src/test/java/seleniumtest/DriverUtil.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverUtil {
+	
+	//
 
 	public static WebDriver getChromeDriver() {
 		String exePath = "C:\\googleChromeDriver\\chromedriver.exe";
